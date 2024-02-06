ab1e646570614403cf85c618df68705f87acd488
RyanHalford <156833391+RyanHalford@users.noreply.github.com>
2024-02-05 19:53:42 -0600

    HW3

--- Program.cs
+++ Program.cs
@@ -1,36 +1,24 @@
-﻿namespace HW2_Part2;
-using System;
+﻿namespace HW3_Part2;
+
 class Program
 {
     static void Main(string[] args)
     {
-    
-         //Got this of the internet and changed up some of the wording to what i needed and a little bit of the code. 
-         //The was orignally a name system not number
-            Console.WriteLine("Enter the first number: ");
-            int n1 = Convert.ToInt32(Console.ReadLine());
+         Console.Write("Enter the value of N: ");
+        int N = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number: ");
-            int n2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the third number: ");
-            int n3 = Convert.ToInt32(Console.ReadLine());
-
-        if(n1<n2){  //Edited this from the test code in the lesson section.
-            if(n1<n3){
-                Console.WriteLine("The smallest num: n1");
+        // Loop through each row
+        for (int i = 0; i < N; i++)
+        {
+            // Loop through each column
+            for (int j = 0; j < N; j++)
+            {
+                // Print the appropriate symbol based on the position
+                Console.Write("#");
             }
-            else{
-                Console.WriteLine("The smallest num: n3");
-            }
-        }
-        else{ 
-            if(n2<n3){
-                Console.WriteLine("The smallest num: n2");
-            }
-            else{
-                Console.WriteLine("The smallest num: n3");
-            }
+            Console.WriteLine(); // Move to the next row
         }
     }
 }
+    
+
